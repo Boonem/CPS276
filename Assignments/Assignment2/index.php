ab176f47af8cbab44b44457796c8e828d1c5dd06
Boonem <mcboone228@gmail.com>
2021-10-04 17:26:20 -0400

    Assignment 2 update (2)

---
+++ Assignments/Assignment2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+<ul>
+    <li><a href="Assignments\Assignment2\Exercise1.php">Exercise 1</a></li>
+    <li><a href="Assignments\Assignment2\webpage.php">Exercise 2</a></li>
+    <li><a href="Assignments\Assignment2\Exercise3.php">Exercise 3</a></li>
+</ul>
+
+</body>
+
+</html>
